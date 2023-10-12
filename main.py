e79888fb39e02c9b63253a345b6191080a01228f
kot9kas <88248720+kot9kas@users.noreply.github.com>
2023-10-12 19:01:04 +0300

    Версия 1.1.0
                       5Sim SMS Receiver ChangeLog
                       ----------------------------------
    
    Версия 1.1.0 -
    ---------------------
    
    [+] Новые функции:
        - Добавлен автоматический парсинг стран из API.
        - Интегрировано автоматическое заполнение операторов и сервисов.
        - Постоянное хранение:
            - Ключ API теперь сохраняется между сессиями для удобства использования.
            - Выбор пользователя по стране, оператору и сервису сохраняется и восстанавливается при следующем запуске.
    
    [!] Улучшения:
        - Улучшена обработка ошибок при взаимодействии с API.
    
    [-] Исправления:
        - Исправлены синтаксические ошибки и некорректное поведение в исходной версии.
        
    [~] Прочее:
        - Оптимизирована структура кода для лучшей поддержки.

--- main.py
+++ main.py
@@ -7,6 +7,60 @@ from PyQt5.QtGui import QFont, QPalette, QColor, QIcon, QPixmap
 from PyQt5.QtCore import Qt, QSize, QSettings
 
 class App(QMainWindow):
+
+    OPERATORS = [
+        "any", "019", "activ", "altel", "beeline", "claro", "ee", "globe", "kcell", 
+        "lycamobile", "matrix", "megafon", "mts", "orange", "pildyk", "play", 
+        "redbullmobile", "rostelecom", "smart", "sun", "tele2", "three", "tigo", 
+        "tmobile", "tnt", "virginmobile", "virtual2", "virtual4", "virtual5", 
+        "virtual7", "virtual8", "virtual12", "virtual15", "virtual16", "virtual17",
+        "virtual18", "virtual19", "virtual20", "virtual21", "virtual22", "virtual23",
+        "virtual24", "virtual25", "virtual26", "virtual27", "virtual28", "vodafone", 
+        "yota", "zz"
+    ]
+
+    SERVICES = [
+        "1688", "23red", "32red", "99app", "ace2three", "adidas", "agroinform", 
+        "airbnb", "airtel", "aitu", "akelni", "alfa", "algida", "alibaba", "aliexpress", 
+        "alipay", "amasia", "amazon", "aol", "astropay", "auchan", "avito", "avon",
+        "azino", "b4ucabs", "baidu", "banqi", "bigolive", "billmill", "bisu", "bitaqaty",
+        "bitclout", "bittube", "blablacar", "blizzard", "blockchain", "blued", "bolt", "brand20ua",
+        "burgerking", "bykea", "cafebazaar", "caixa", "careem", "carousell", "cdkeys", "cekkazan",
+        "citaprevia", "citymobil", "clickentregas", "cliqq", "clubhouse", "cmtcuzdan", "coinbase",
+        "coinfield", "craigslist", "cryptocom", "dbrua", "deliveroo", "delivery", "dent", "dhani",
+        "didi", "digikala", "discord", "disneyhotstar", "divar", "dixy", "dodopizza", "domdara",
+        "dominospizza", "dostavista", "douyu", "dream11", "drom", "drugvokrug", "dukascopy", "easypay",
+        "ebay", "ebikegewinnspiel", "edgeless", "electroneum", "eneba", "ezbuy", "faberlic", "facebook",
+        "fiqsy", "fiverr", "foodpanda", "foody", "forwarding", "freecharge", "galaxy", "gamearena",
+        "gameflip", "gamekit", "gamer", "gcash", "get", "getir", "gett", "gg", "gittigidiyor",
+        "global24", "globaltel", "globus", "glovo", "google", "grabtaxi", "green", "grindr",
+        "hamrahaval", "happn", "haraj", "hepsiburadacom", "hezzl", "hily", "hopi", "hqtrivia",
+        "humblebundle", "humta", "huya", "icard", "icq", "icrypex", "ifood", "immowelt", "imo",
+        "inboxlv", "indriver", "ininal", "instagram", "iost", "iqos", "ivi", "iyc", "jd", "jkf",
+        "justdating", "justdial", "kakaotalk", "karusel", "keybase", "komandacard", "kotak811",
+        "kucoinplay", "kufarby", "kvartplata", "kwai", "lazada", "lbry", "lenta", "lianxin", "line",
+        "linkedin", "livescore", "magnit", "magnolia", "mailru", "mamba", "mcdonalds", "meetme",
+        "mega", "mercado", "michat", "microsoft", "miloan", "miratorg", "mobile01", "momo",
+        "monese", "monobank", "mosru", "mrgreen", "mtscashback", "myfishka", "myglo", "mylove",
+        "mymusictaste", "mzadqatar", "nana", "naver", "ncsoft", "netflix", "nhseven", "nifty",
+        "nike", "nimses", "nrjmusicawards", "nttgame", "odnoklassniki", "offerup", "offgamers",
+        "okcupid", "okey", "okta", "olacabs", "olx", "onlinerby", "openpoint", "oraclecloud",
+        "oriflame", "other", "ozon", "paddypower", "pairs", "papara", "paxful", "payberry",
+        "paycell", "paymaya", "paypal", "paysend", "paytm", "peoplecom", "perekrestok", "pgbonus",
+        "picpay", "pof", "pokec", "pokermaster", "potato", "powerkredite", "prajmeriz2020",
+        "premiumone", "prom", "proton", "protonmail", "protp", "pubg", "pureplatfrom", "pyaterochka",
+        "pyromusic", "q12trivia", "qiwiwallet", "quipp", "rakuten", "rambler", "rediffmail", "reuse",
+        "ripkord", "rosakhutor", "rsa", "rutube", "samokat", "seosprint", "sheerid", "shopee",
+        "signal", "sikayetvar", "skout", "snapchat", "snappfood", "sneakersnstuff", "socios",
+        "sportmaster", "spothit", "ssoidnet", "steam", "surveytime", "swvl", "taksheel", "tango",
+        "tantan", "taobao", "telegram", "tencentqq", "ticketmaster", "tiktok", "tinder", "tosla",
+        "totalcoin", "touchance", "trendyol", "truecaller", "twitch", "twitter", "uber", "ukrnet",
+        "uploaded", "vernyi", "vernyj", "viber", "vitajekspress", "vkontakte", "voopee", "wechat",
+        "weibo", "weku", "weststein", "whatsapp", "wildberries", "wingmoney", "winston", "wish",
+        "wmaraci", "wolt", "yaay", "yahoo", "yalla", "yandex", "yemeksepeti", "youdo", "youla",
+        "youstar", "zalo", "zoho", "zomato"
+    ]
+
     def __init__(self):
         super().__init__()
     
@@ -54,6 +108,8 @@ class App(QMainWindow):
 
         self.apiKeyLabel = QLabel('API Ключ:')
         self.apiKeyEdit = QLineEdit(self)
+        self.apiKeyEdit.setText(self.settings.value("api_key", ""))
+        self.apiKeyEdit.textChanged.connect(self.saveApiKey)
         
         self.balanceLabel = QLabel("Баланс: -")
         balanceFont = QFont()
@@ -95,17 +151,29 @@ class App(QMainWindow):
         numGroupBox = QGroupBox("Детали номера")
         numGroupBox.setStyleSheet(groupBoxStyle)
         numLayout = QGridLayout()
-
+        # Страны
         self.countryLabel = QLabel('Страна:')
         self.countryCombo = QComboBox(self)
-        self.countryCombo.addItems(['Afghanistan'])
+        self.load_countries()
+    
+        # Оператор
         self.operatorLabel = QLabel('Оператор:')
         self.operatorCombo = QComboBox(self)
-        self.operatorCombo.addItems(['Any', 'iTelecom'])
+        self.operatorCombo.addItems(self.OPERATORS)
+
+        # Продукт
         self.productLabel = QLabel('Продукт:')
         self.productCombo = QComboBox(self)
-        self.productCombo.addItems(['Amazon'])
+        self.productCombo.addItems(self.SERVICES)
+
         self.buyButton = QPushButton('Купить номер', self)
+        
+        self.load_settings()
+
+        self.countryCombo.currentIndexChanged.connect(self.save_settings)
+        self.operatorCombo.currentIndexChanged.connect(self.save_settings)
+        self.productCombo.currentIndexChanged.connect(self.save_settings)
+
         self.buyButton.setStyleSheet("""
             QPushButton {
                 background-color: #3f51b5;
@@ -311,6 +379,50 @@ class App(QMainWindow):
             }
         """
         self.setStyleSheet(dark_style)
+
+    def saveApiKey(self):
+        api_key = self.apiKeyEdit.text()
+        self.settings.setValue("api_key", api_key)
+
+    def load_countries(self):
+        api_key = self.apiKeyEdit.text()
+        headers = {
+            'Authorization': 'Bearer ' + api_key,
+            'Accept': 'application/json',
+        }
+        response = requests.get('https://5sim.net/v1/guest/countries', headers=headers)
+        if response.status_code == 200:
+            countries = response.json()
+            self.countryCombo.addItems(countries)
+        else:
+            pass
+
+
+    def save_settings(self):
+        self.settings.setValue("selected_country", self.countryCombo.currentText())
+        self.settings.setValue("selected_operator", self.operatorCombo.currentText())
+        self.settings.setValue("selected_service", self.productCombo.currentText())
+
+    def load_settings(self):
+        selected_country = self.settings.value("selected_country", "")
+        selected_operator = self.settings.value("selected_operator", "")
+        selected_service = self.settings.value("selected_service", "")
+
+        if selected_country:
+            index_country = self.countryCombo.findText(selected_country)
+            if index_country != -1:
+                self.countryCombo.setCurrentIndex(index_country)
+
+        if selected_operator:
+            index_operator = self.operatorCombo.findText(selected_operator)
+            if index_operator != -1:
+                self.operatorCombo.setCurrentIndex(index_operator)
+
+        if selected_service:
+            index_service = self.productCombo.findText(selected_service)
+            if index_service != -1:
+                self.productCombo.setCurrentIndex(index_service)
+
 
 
 if __name__ == '__main__':
